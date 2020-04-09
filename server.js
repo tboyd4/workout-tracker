04a3e2fae33e0924daa1ebafd673f54c9b2edd2d
tboyd4 <tboyd1898@gmail.com>
2020-04-09 10:24:54 -0500

    added 2 additional routes for other html files

--- server.js
+++ server.js
@@ -12,6 +12,14 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
+app.get('/exercise', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/exercise.html'));
+});
+
+app.get('/stats', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/stats.html'));
+});
+
 // listen command
 app.listen(PORT, () => {
     console.log(`Listening on Port ${PORT}`);

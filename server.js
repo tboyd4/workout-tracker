62d2e0099c525ffd54bec596c3e030af55612192
tboyd4 <tboyd1898@gmail.com>
2020-04-11 17:10:47 -0500

    added additional routes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // variable and import declarations
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const Exercise = require("./models/workouts");
@@ -8,26 +8,28 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 // middleware expressions
-app.use(express.static('public'))
+app.use(express.static("public"));
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userdb", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userdb", {
+  useNewUrlParser: true,
+});
 
 // routes
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/public/index.html'));
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname + "/public/index.html"));
 });
 
-app.get('/exercise', function(req, res) {
-    res.sendFile(path.join(__dirname + '/public/exercise.html'));
+app.get("/exercise", function (req, res) {
+  res.sendFile(path.join(__dirname + "/public/exercise.html"));
 });
 
-app.get('/stats', function(req, res) {
-    res.sendFile(path.join(__dirname + '/public/stats.html'));
+app.get("/stats", function (req, res) {
+  res.sendFile(path.join(__dirname + "/public/stats.html"));
 });
 
 // listen command
 app.listen(PORT, () => {
-    console.log(`Listening on Port ${PORT}`);
-})
+  console.log(`Listening on Port ${PORT}`);
+});
